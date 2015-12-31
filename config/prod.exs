a2881928cb6bb4f678bfe04f477495255967354d
Mike North <michael.l.north@gmail.com>
2015-12-31 11:14:17 -0700

    remove static manifest reference

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :mikeworks, Mikeworks.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json",
+  # cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :mikeworks, Mikeworks.Repo,
